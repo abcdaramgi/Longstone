247b25fdadc5722d76bdd107c0cf648b1058ed9a
abcdaramgi <86977236+abcdaramgi@users.noreply.github.com>
2022-10-15 05:42:01 +0900

    [Server] - for CI test

--- server/Board/src/test/java/com/example/demo/BoardApplicationTests.java
+++ server/Board/src/test/java/com/example/demo/BoardApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+// package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BoardApplicationTests {
+// @SpringBootTest
+// class BoardApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
