196d291a8b198dcc08b13c4bf03074ff8166842d
dhkim1206 <69470424+dhkim1206@users.noreply.github.com>
2022-10-15 04:17:54 +0900

    [Server] - Test

--- server/Board/src/main/java/com/example/demo/BoardApplication.java
+++ server/Board/src/main/java/com/example/demo/BoardApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoardApplication {
 
 	public static void main(String[] args) {
-		System.out.println("*******************Godlen Hour Server*******************");
+		System.out.println("*********************Godlen Hour Server*******************");
 	}
 
 }
