592043575c0d95cc30739f8498b9e2215cd107f5
abcdaramgi <86977236+abcdaramgi@users.noreply.github.com>
2022-10-15 05:44:23 +0900

    [Server] - for CI test

--- server/Board/src/main/java/com/example/demo/BoardApplication.java
+++ server/Board/src/main/java/com/example/demo/BoardApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoardApplication {
 
 	public static void main(String[] args) {
-		System.out.println("*************************plzplzplzplzplz*******************");
+		System.out.println("*************************ddddddddddddplz*******************");
 	}
 
 }
