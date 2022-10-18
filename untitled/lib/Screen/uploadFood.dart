1f7f7d8f4fe5408ea35477d5788a0f6aa1d71946
hyew0nn <113279618+hyew0nn@users.noreply.github.com>
2022-10-18 13:40:33 +0900

    newFlutter
    .

---
+++ untitled/lib/Screen/uploadFood.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class UploadfoodPage extends StatelessWidget {
+  const UploadfoodPage({Key? key}) : super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('음식등록', style: TextStyle(color: Colors.black),),
+          // 가운데 정렬
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          // appBar 밑 그림자 제거,
+          elevation: 0.0,
+        )
+    );
+  }
+}
