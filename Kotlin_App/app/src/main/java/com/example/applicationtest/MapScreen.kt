cbbdd9f689cd14560ef07d74714590b9f5991292
abcdaramgi <wwa1102@naver.com>
2022-11-03 17:13:16 +0900

    [App] - kakao map lib

--- Kotlin_App/app/src/main/java/com/example/applicationtest/MapScreen.kt
+++ Kotlin_App/app/src/main/java/com/example/applicationtest/MapScreen.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.MapView
+import org.jetbrains.annotations.Nullable
 
 class MapScreen : Fragment() {
+    private lateinit var mapView: MapView
 
     companion object {
         fun newInstance(): MapScreen {
@@ -31,4 +34,44 @@ class MapScreen : Fragment() {
         return inflater.inflate(R.layout.fragment_map_screen, container, false)
     }
 
+    override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView = view.findViewById(R.id.map_view)
+        mapView.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
 }
