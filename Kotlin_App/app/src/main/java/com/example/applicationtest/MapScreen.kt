59c4de17d5ad6fda2d2af47cc6827a98657a9909
abcdaramgi <wwa1102@naver.com>
2022-11-10 21:46:14 +0900

    [Kotlin] - 좌표찍기

--- Kotlin_App/app/src/main/java/com/example/applicationtest/MapScreen.kt
+++ Kotlin_App/app/src/main/java/com/example/applicationtest/MapScreen.kt
@@ -1,16 +1,33 @@
 package com.example.applicationtest
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
+import com.example.applicationtest.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.fragment_map_screen.*
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 import org.jetbrains.annotations.Nullable
 
 class MapScreen : Fragment() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var mapView: MapView
+    private val ACCESS_FINE_LOCATION = 1000
 
     companion object {
         fun newInstance(): MapScreen {
@@ -39,10 +56,42 @@ class MapScreen : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val v: View = inflater.inflate(R.layout.fragment_map_screen, container, false)
-
+        //binding = ActivityMainBinding.inflate(layoutInflater)
         val mapView = MapView(getActivity())
         val mapViewContainer: ViewGroup = v.findViewById(R.id.map_view) as ViewGroup
         mapViewContainer.addView(mapView)
+
+
+/*        map_page_location_btn.setOnClickListener {
+            val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                val lm: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                try {
+                    val userNowLocation: Location? =
+                        lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                    val uLatitude = userNowLocation.latitude
+                    val uLongitude = userNowLocation.longitude
+                    val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude, uLongitude)
+                    mapView.setMapCenterPoint(uNowPosition, true)
+                }catch(e: NullPointerException){
+                    Log.e("LOCATION_ERROR", e.toString())
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                        ActivityCompat.finishAffinity(this)
+                    }else{
+                        ActivityCompat.finishAffinity(this)
+                    }
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    System.exit(0)
+                }
+            }else{
+                Toast.makeText(this, "위치 권한이 없습니다.", Toast.LENGTH_SHORT).show()
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE )
+            }
+        }
+*/
+        startTracking()
         return v
     }
 
@@ -51,6 +100,11 @@ class MapScreen : Fragment() {
 /*        mapView = view.findViewById(R.id.map_view)
         mapView.onCreate(savedInstanceState)*/
     }
+
+    private fun startTracking() {
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(35.798838, 128.583052), true);
+    }
+
 /*
 
     override fun onStart() {
