45bbb56a5d7793a6ed9dad19e82f7467f53d5074
cej3827 <56065660+cej3827@users.noreply.github.com>
2022-11-24 17:53:10 +0900

    imageView에 사진 띄우기

--- Kotlin_App/app/src/main/java/com/example/applicationtest/StAddFragment.kt
+++ Kotlin_App/app/src/main/java/com/example/applicationtest/StAddFragment.kt
@@ -1,38 +1,47 @@
 package com.example.applicationtest
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.DialogInterface
+import android.content.Intent
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.SurfaceControl.Transaction
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.NumberPicker
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.applicationtest.DTO.postDTO
 import com.example.applicationtest.Transport.PostTask
 import kotlinx.android.synthetic.main.fragment_st_add.*
 import kotlinx.android.synthetic.main.item_alertdialog.view.*
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
 
 class StAddFragment : AppCompatActivity() {
     private var send_btn: Button? = null
-    private var price_edit: EditText? = null
+    private var original_price_edit: EditText? = null
+    private var discount_price_edit: EditText? = null
     private var foodName_edit: EditText? = null
+    private var food_count: EditText? = null
+    private var img_btn: Button? = null
     var items: List<postDTO>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_st_add)
         timer()
+        img_add()
+        add()
 
-<<<<<<< Updated upstream
         btn_registration.setOnClickListener({
             val builder = AlertDialog.Builder(this)
             builder.setMessage("등록되었습니다.")
@@ -42,7 +51,6 @@ class StAddFragment : AppCompatActivity() {
                     })
             builder.show()
         })
-        add()
     }
     //NumberPicker 활성화
     fun timer(){
@@ -60,14 +68,11 @@ class StAddFragment : AppCompatActivity() {
     
     @SuppressLint("MissingInflatedId")
     fun add() {
-        send_btn = findViewById(R.id.button)
-        price_edit = findViewById(R.id.editTextOrigianlPrice)
+        send_btn = findViewById(R.id.btn_registration)
+        original_price_edit = findViewById(R.id.editTextOrigianlPrice)
         foodName_edit = findViewById(R.id.editTextFoodName)
-=======
-        send_btn = getView()?.findViewById(R.id.button)
-        price_edit = getView()?.findViewById(R.id.editTextOrigianlPrice)
-        foodName_edit = getView()?.findViewById(R.id.editTextFoodName)
->>>>>>> Stashed changes
+        discount_price_edit = findViewById((R.id.editTextDiscountPrice))
+        food_count = findViewById((R.id.editTextNumber))
 
         items = ArrayList()
 
@@ -77,19 +82,76 @@ class StAddFragment : AppCompatActivity() {
                 post()
             })
         }
+
     }
     
     fun post() {
         try {
-            val price = price_edit!!.text.toString()
+            val originalPrice = original_price_edit!!.text.toString()
+            var discountPrice = discount_price_edit!!.text.toString()
+            var foodCount = food_count!!.text.toString()
             val foodName = foodName_edit!!.text.toString()
 
-            Log.d("앱에서 보낸값", "$price, $foodName")
+            Log.d("앱에서 보낸값", "$originalPrice, $discountPrice, $foodCount $foodName")
             val task = PostTask()
-            val result = task.execute(price, foodName).get()
+            val result = task.execute(originalPrice, discountPrice, foodCount, foodName).get()
             Log.d("받은값", result)
             Log.d("post", "posting end...")
+
         } catch (e: Exception) {
+        }
+    }
+
+    fun img_add() {
+        //이미지등록
+        img_btn = findViewById(R.id.button2)
+        img_btn?.setOnClickListener {
+            openGallery()
+        }
+    }
+
+    private fun openGallery(){
+        Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "image/*"
+            startActivityForResult(
+                Intent.createChooser(this, "Get Album"),
+                102
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+
+        if (requestCode == 102 && resultCode == Activity.RESULT_OK){
+            var currentImageURL = intent?.data
+            // Base64 인코딩부분
+            val ins: InputStream? = currentImageURL?.let {
+                applicationContext?.contentResolver?.openInputStream(
+                    it
+                )
+            }
+            val img: Bitmap = BitmapFactory.decodeStream(ins)
+            ins?.close()
+            val resized = Bitmap.createScaledBitmap(img, 256, 256, true)
+            val byteArrayOutputStream = ByteArrayOutputStream()
+            resized.compress(Bitmap.CompressFormat.JPEG, 60, byteArrayOutputStream)
+            val byteArray: ByteArray = byteArrayOutputStream.toByteArray()
+            val outStream = ByteArrayOutputStream()
+            val res: Resources = resources
+            var profileImageBase64 = Base64.encodeToString(byteArray, Base64.NO_WRAP)
+            // 여기까지 인코딩 끝
+
+            // 이미지 뷰에 선택한 이미지 출력
+            val imageview: ImageView?= findViewById(R.id.imageView)
+            imageview?.setImageURI(currentImageURL)
+            try {
+                //이미지 선택 후 처리
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        } else{
+            Log.d("ActivityResult", "something wrong")
         }
     }
 
