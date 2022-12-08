56f328c706e10df931ca9250e5a8a307e11a0da5
SMHyeon <thsaudgus72@gmail.com>
2022-12-09 03:51:43 +0900

    [App] UI 수정 + Icon 교체

--- Kotlin_App/app/src/main/java/com/example/applicationtest/StAddFragment.kt
+++ Kotlin_App/app/src/main/java/com/example/applicationtest/StAddFragment.kt
@@ -76,11 +76,6 @@ class StAddFragment : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_st_add_object)
 
-        setSupportActionBar(st_toolbar_SalesAdd) //커스텀한 toolbar 액션바로 사용
-        supportActionBar?.setDisplayShowTitleEnabled(false)
-        //액션바에 표시되는 제목의 표시유무를 설정합니다. false로 해야 custom한 툴바의 이름이 화면에 보이게 됩니다.
-        supportActionBar?.setDisplayHomeAsUpEnabled(true) //액션바 뒤로가기 아이콘 표시
-
         timer()
 //        img_add()
 //        add()
