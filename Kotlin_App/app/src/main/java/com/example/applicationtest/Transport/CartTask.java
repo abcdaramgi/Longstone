1925d55c152380fc16434f6d275f76117c8dbcd6
JIK-K <kddnswlr@naver.com>
2022-12-10 02:02:00 +0900

    [server] 자새로이

--- Kotlin_App/app/src/main/java/com/example/applicationtest/Transport/CartTask.java
+++ Kotlin_App/app/src/main/java/com/example/applicationtest/Transport/CartTask.java
@@ -21,7 +21,7 @@ public class CartTask extends AsyncTask<String, Void, String> {
     protected String doInBackground(String... strings) {
         try{
             String str;
-            URL url = new URL("http://10.0.2.2:8080/post/cart");
+            URL url = new URL("http://ec2-3-35-255-89.ap-northeast-2.compute.amazonaws.com/post/cart");
 
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 //                conn.setRequestProperty("Content-Type", "application/x-www-form-unlencoded");
