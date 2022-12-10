d7ef73c07813cb4235860842346f9c51851352d6
JIK-K <kddnswlr@naver.com>
2022-12-11 00:28:56 +0900

    [server] 상품삭제마저다함구현

--- Kotlin_App/app/src/main/java/com/example/applicationtest/Transport/deleteFoodTask.java
+++ Kotlin_App/app/src/main/java/com/example/applicationtest/Transport/deleteFoodTask.java
@@ -3,6 +3,9 @@ package com.example.applicationtest.Transport;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -17,7 +20,7 @@ public class deleteFoodTask extends AsyncTask<String, Void, String>{
     protected String doInBackground(String... strings) {
         try{
             String str;
-            URL url = new URL("http://ec2-3-35-255-89.ap-northeast-2.compute.amazonaws.com/post/deleteStoreFood");
+            URL url = new URL("http://222.103.14.187:8080/post/deleteStoreFood");
 
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestProperty("Content-Type", "application/json");
@@ -28,10 +31,12 @@ public class deleteFoodTask extends AsyncTask<String, Void, String>{
             //서버에 보낼값포함해 요청함
             OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
 
-            String sendData = strings[0];
+            JSONObject sendJson = new JSONObject();
+            sendJson.put("sellerId", strings[0]);
+            sendJson.put("pdname", strings[1]);
 
-            osw.write(sendData);
-            Log.d("pdName :", sendData);
+            osw.write(sendJson.toString());
+            Log.d("pdName :", sendJson.toString());
             osw.flush();
 
 
@@ -50,6 +55,8 @@ public class deleteFoodTask extends AsyncTask<String, Void, String>{
         }catch (MalformedURLException e){
             e.printStackTrace();
         }catch (IOException e){
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
         return receiveMsg;
