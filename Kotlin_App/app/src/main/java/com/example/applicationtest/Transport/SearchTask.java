cfb01354e91d1adeb55df4b6daf0469b71d08a46
JIK-K <kddnswlr@naver.com>
2022-12-10 18:47:34 +0900

    [server] once again

--- Kotlin_App/app/src/main/java/com/example/applicationtest/Transport/SearchTask.java
+++ Kotlin_App/app/src/main/java/com/example/applicationtest/Transport/SearchTask.java
@@ -23,7 +23,7 @@ public class SearchTask extends AsyncTask<String, Void, String> {
     protected String doInBackground(String... strings) {
         try {
             String str;
-            URL url = new URL("http://222.103.14.187:8080/search/topic");
+            URL url = new URL("http://ec2-3-35-255-89.ap-northeast-2.compute.amazonaws.com/search/topic");
 
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestProperty("Content-Type", "application/json");
